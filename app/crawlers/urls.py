27c6f9a74d19528461313cc1918764984f7243f1
chajeehyung <chajee308@gmail.com>
2018-11-16 19:13:38 +0900

    project 완성

--- app/crawlers/urls.py
+++ app/crawlers/urls.py
@@ -7,4 +7,5 @@ app_name = 'posts'
 urlpatterns = [
     path('image-list/', views.image_list, name='image-list'),
     path('image-search/', views.image_search, name='image-search'),
+    path('save-image/', views.save_image, name='save-image'),
 ]
--- app/crawlers/views.py
+++ app/crawlers/views.py
@@ -1,12 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup
+import urllib.request
+import os
 import requests
 
 
 def image_search(request):
     search_keyword = request.GET.get('search_keyword')
     # return redirect('image_list', search_keyword)
-    return image_list(request,search_keyword)
+    return image_list(request, search_keyword)
 
 def image_list(request, search_keyword=''):
     url_prefix = 'https://www.google.co.kr/search?q='
@@ -22,8 +25,23 @@ def image_list(request, search_keyword=''):
     for col in col_list:
         urls.append(col.get('src'))
 
+    urls.pop(0)
     context = {
         'urls': urls
     }
 
     return render(request, 'posts/post_list.html', context)
+
+
+def save_image(request):
+    urls = request.POST.getlist('urls')
+    # print(request.POST.getlist('urls'))
+    # urls.append(request.POST.get('urls')[0])
+    num = 0
+    for url in urls:
+
+        urllib.request.urlretrieve(url, f'temp-{num}.png')
+        num +=1
+
+    return redirect('posts:image-list')
+
